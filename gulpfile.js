8bab2370da8c5b163415dc6b4371d94282f024a5
chemdemo <yangdemo@gmail.com>
2014-08-26 21:18:11 +0800

    use gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
