ed8ec05858ebb0fa6666f2faee5521f6f5920186
chemdemo <yangdemo@gmail.com>
2014-08-26 21:30:31 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,9 @@ var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
-var rev = require('gulp-rev');
+
+var conf = {
+    paths: {
+        'root': ''
+    }
+};
